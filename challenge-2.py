3601068829f158e3368de087f90b7fb5c12c2d9b
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-09-01 21:11:41 +0900

    challenge-2

---
+++ challenge-2.py
@@ -0,0 +1,31 @@
+from operator import truediv
+
+
+playing = True
+
+
+# a = int(input("Choose a number:\n"))
+# b = int(input("Choose another one:\n"))
+# operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
+  
+# add your code here!
+
+while playing:
+    a = int(input("Choose a number:\n"))
+    b = int(input("Choose another one:\n"))
+    operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
+    if operation == "+":
+        print(a+b)
+        continue
+    elif operation =="-":
+        print(a-b)
+        continue
+    elif operation =="*":
+        print(a*b)
+        continue
+    elif operation =="/":
+        print(a/b)
+        continue
+    elif operation =="exit":
+        break
+
